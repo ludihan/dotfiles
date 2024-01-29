84ccf483fbe7241e18293b03cf8e8eb2675d9dbe
ludihan <65617704+ludihan@users.noreply.github.com>
2024-01-29 15:07:00 -0400

    2024-01-29

--- .config/nvim/lua/ludihan/config.lua
+++ .config/nvim/lua/ludihan/config.lua
@@ -84,7 +84,6 @@ require("nvim-tree").setup({
     },
     actions = {
         open_file = {
-            quit_on_open = true,
             window_picker = {
                 enable = false
             }
