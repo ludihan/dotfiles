33070909044771a4d29b509f6a472e5a8bae139d
ludihan <65617704+ludihan@users.noreply.github.com>
2024-05-02 23:09:12 -0300

    2024-05-02

--- .config/nvim/lua/ludihan/config.lua
+++ .config/nvim/lua/ludihan/config.lua
@@ -74,6 +74,7 @@ require('mason-lspconfig').setup({
 
 -- remaps
 vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
