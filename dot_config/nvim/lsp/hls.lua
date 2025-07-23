f551e919e67b618f49b40b3294ca9ac8c0440700
ludihan <65617704+ludihan@users.noreply.github.com>
2025-07-23 14:50:02 -0300

    Update .config/nvim/init.lua Update .config/nvim/lsp/hls.lua

--- dot_config/nvim/lsp/hls.lua
+++ dot_config/nvim/lsp/hls.lua
@@ -1,16 +1,16 @@
-local util = require 'lspconfig.util'
+local util = require "lspconfig.util"
 
 return {
-    cmd = { 'haskell-language-server-wrapper', '--lsp' },
-    filetypes = { 'haskell', 'lhaskell', 'cabal' },
+    cmd = { "haskell-language-server-wrapper", "--lsp" },
+    filetypes = { "haskell", "lhaskell", "cabal" },
     root_dir = function(bufnr, on_dir)
         local fname = vim.api.nvim_buf_get_name(bufnr)
-        on_dir(util.root_pattern('hie.yaml', 'stack.yaml', 'cabal.project', '*.cabal', 'package.yaml')(fname))
+        on_dir(util.root_pattern("hie.yaml", "stack.yaml", "cabal.project", "*.cabal", "package.yaml")(fname))
     end,
     settings = {
         haskell = {
-            formattingProvider = 'fourmolu',
-            cabalFormattingProvider = 'cabal-fmt',
+            formattingProvider = "fourmolu",
+            cabalFormattingProvider = "cabal-fmt",
             plugin = {
                 rename = {
                     config = {
