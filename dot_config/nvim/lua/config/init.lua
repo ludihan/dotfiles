8318d7cfb8f8592023cdb2835289025c74cd476b
ludihan <65617704+ludihan@users.noreply.github.com>
2025-07-23 14:51:34 -0300

    Remove .config/nvim/.luarc.json Remove .config/nvim/init.lua Remove .config/nvim/lazy-lock.json Remove .config/nvim/lsp/hls.lua Remove .config/nvim/lua/config.lua Remove .config/nvim/lua/config/autocmd.lua Remove .config/nvim/lua/config/init.lua Remove .config/nvim/lua/config/keymap.lua Remove .config/nvim/lua/config/lazy.lua Remove .config/nvim/lua/config/option.lua Remove .config/nvim/lua/.luarc.json Remove .config/nvim/lua/plugins.lua Remove .config/nvim/lua/plugins/bigfile.lua Remove .config/nvim/lua/plugins/gruvbox-material.lua Remove .config/nvim/lua/plugins/mason-lspconfig.lua Remove .config/nvim/lua/plugins/nvim-lspconfig.lua Remove .config/nvim/lua/plugins/nvim-treesitter.lua Remove .config/nvim/lua/plugins/nvim-ts-autotag.lua Remove .config/nvim/lua/plugins/oil.lua Remove .config/nvim/lua/plugins/rainbow-delimiters.lua Remove .config/nvim/lua/plugins/telescope.lua

--- dot_config/nvim/lua/config/init.lua
+++
@@ -1,4 +0,0 @@
-require("config.option")
-require("config.keymap")
-require("config.autocmd")
-require("config.lazy")
--- dot_config/nvim/lua/plugins/oil.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "stevearc/oil.nvim",
-    opts = {},
-    keys = {
-        { "-", "<CMD>Oil<CR>", { desc = "Open directory (oil)" } }
-    }
-}
