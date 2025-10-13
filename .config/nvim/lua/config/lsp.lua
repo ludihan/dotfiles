db140ff0b59e66ea610725d9204e580ac39a14b5
ludihan <65617704+ludihan@users.noreply.github.com>
2025-10-13 19:59:20 -0300

    readd haskell configuration

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,5 +1,23 @@
+local util = require 'lspconfig.util'
 -- Julia
 vim.lsp.enable('julials')
+
+-- Haskell
+vim.lsp.config('hls', {
+    cmd = { 'haskell-language-server-wrapper', '--lsp' },
+    filetypes = { 'haskell', 'lhaskell' },
+    root_dir = function(bufnr, on_dir)
+        local fname = vim.api.nvim_buf_get_name(bufnr)
+        on_dir(util.root_pattern('hie.yaml', 'stack.yaml', 'cabal.project', '*.cabal', 'package.yaml')(fname))
+    end,
+    settings = {
+        haskell = {
+            formattingProvider = 'fourmolu',
+            cabalFormattingProvider = 'cabalfmt',
+        },
+    },
+})
+vim.lsp.enable('hls')
 
 -- Vue
 local vue_language_server_path = vim.fn.expand '$MASON/packages' ..
