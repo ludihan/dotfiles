16834958df15c4312cf15c2a0501343ba83cd821
ludihan <65617704+ludihan@users.noreply.github.com>
2026-06-13 16:59:52 -0400

    remove reflex cache

--- nixos/default.nix
+++ nixos/default.nix
@@ -74,10 +74,6 @@
         flake-registry = "";
         # Workaround for https://github.com/NixOS/nix/issues/9574
         nix-path = config.nix.nixPath;
-
-        # obelisk
-        substituters = [ "https://nixcache.reflex-frp.org" ];
-        trusted-public-keys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
       };
       # Opinionated: disable channels
       channel.enable = false;
