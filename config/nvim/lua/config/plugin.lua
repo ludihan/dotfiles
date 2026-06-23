00ddd2059936d37e70667313e92cc46354606c3c
ludihan <65617704+ludihan@users.noreply.github.com>
2026-06-23 19:23:27 -0400

    add blink

--- config/nvim/lua/config/plugin.lua
+++ config/nvim/lua/config/plugin.lua
@@ -8,8 +8,43 @@ vim.pack.add({
     'https://github.com/folke/snacks.nvim',
     'https://github.com/nvim-treesitter/nvim-treesitter',
     'https://github.com/windwp/nvim-ts-autotag',
-    -- 'https://github.com/scalameta/nvim-metals',
-    -- 'https://github.com/elixir-tools/elixir-tools.nvim',
+    'https://github.com/Saghen/blink.cmp',
+    'https://github.com/saghen/blink.lib',
+})
+
+require('blink.cmp').setup({
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = { preset = 'default' },
+
+    -- (Default) Only show the documentation popup when manually triggered
+    completion = {
+        documentation = { auto_show = true },
+        menu = {
+            draw = {
+                columns = { { 'label', 'label_description', gap = 1 }, { 'kind' } }
+            }
+        }
+    },
+
+    -- (Default) list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
+
+    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"`
+    -- See the fuzzy documentation for more information
+    fuzzy = { implementation = "lua" }
 })
 
 local ts = require('nvim-treesitter')
